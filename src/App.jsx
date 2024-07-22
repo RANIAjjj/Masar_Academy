fa51e8c6253c7bb42d276be134f177683dc52ad9
RANIAjjj <ranyamohamed381@gmail.com>
2024-07-22 19:01:17 +0200

    1st commit in test repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Subscribtion from './components/Subscribtion'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Subscribtion/>
+    </>
+  )
+}
+
+export default App
